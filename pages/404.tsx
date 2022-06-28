8cd88350f6943ffd5ed4593306d7ff5bd4fdc3a2
lithiumsurfer <alihaydar.keten@gmail.com>
2022-06-28 23:52:08 +0300

    add error page

---
+++ pages/404.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+function Error() {
+  return (
+    <div>
+      <Head>
+        <title>404</title>
+      </Head>
+      <div>
+        <p>404</p>
+      </div>
+    </div>
+  );
+}
+
+export default Error;
